fdc3abc06e786edc5d7290975ba9354ad24cbd35
Iqbal Hassan <102966946+iqbalhassan-021@users.noreply.github.com>
2023-12-21 17:42:28 +0500

    commit

--- src/assets/js/code.js
+++ src/assets/js/code.js
@@ -1,45 +1,48 @@
 var count = 1;
-function hideMainForm(){
-    document.getElementById('tab_left').style.backgroundColor = "#D9D7D7";
-    document.getElementById('tab_right').style.backgroundColor = "#FD7605";
-    document.getElementById('mainForm').style.display = "none";
-    document.getElementById('mainFormhidden').style.display = "block";
-    
+function hideMainForm() {
+  document.getElementById("tab_left").style.backgroundColor = "#D9D7D7";
+  document.getElementById("tab_right").style.backgroundColor = "#FD7605";
+  document.getElementById("mainForm").style.display = "none";
+  document.getElementById("mainFormhidden").style.display = "block";
 }
-function dispMainForm(){
-    document.getElementById('tab_left').style.backgroundColor = "#FD7605";
-    document.getElementById('tab_right').style.backgroundColor = "#D9D7D7";
-    document.getElementById('mainFormhidden').style.display = "none";
-    document.getElementById('mainForm').style.display = "block";
-    
+function dispMainForm() {
+  document.getElementById("tab_left").style.backgroundColor = "#FD7605";
+  document.getElementById("tab_right").style.backgroundColor = "#D9D7D7";
+  document.getElementById("mainFormhidden").style.display = "none";
+  document.getElementById("mainForm").style.display = "block";
 }
-function show_hideNav(){
-    if(count%2 == 0){
-        document.getElementById('nav').style.display = "flex";
-    }
-    else if(count%2 == 1){
-        document.getElementById('nav').style.display = "none";
-    }
+function show_hideNav() {
+  if (count % 2 == 0) {
+    document.getElementById("nav").style.display = "flex";
+  } else if (count % 2 == 1) {
+    document.getElementById("nav").style.display = "none";
+  }
 
-    count = count + 1;
+  count = count + 1;
 }
-function verticalScrollBottom(){
-    const divToBeScrolled = document.getElementById('lastdiv');
-    divToBeScrolled.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+function verticalScrollBottom() {
+  const divToBeScrolled = document.getElementById("lastdiv");
+  divToBeScrolled.scrollIntoView({
+    behavior: "smooth",
+    block: "nearest",
+    inline: "start",
+  });
 }
-function verticalScrollup(){
-    const divToBeScrolled = document.getElementById('firstdiv');
-    divToBeScrolled.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+function verticalScrollup() {
+  const divToBeScrolled = document.getElementById("firstdiv");
+  divToBeScrolled.scrollIntoView({
+    behavior: "smooth",
+    block: "nearest",
+    inline: "start",
+  });
 }
 
+function show_mobiletab() {
+  if (count % 2 == 0) {
+    document.getElementById("mobiletabmanue").style.display = "flex";
+  } else if (count % 2 == 1) {
+    document.getElementById("mobiletabmanue").style.display = "none";
+  }
 
-function show_mobiletab(){
-    if(count%2 == 0){
-        document.getElementById('mobiletabmanue').style.display = "flex";
-    }
-    else if(count%2 == 1){
-        document.getElementById('mobiletabmanue').style.display = "none";
-    }
-
-    count = count + 1;
+  count = count + 1;
 }
